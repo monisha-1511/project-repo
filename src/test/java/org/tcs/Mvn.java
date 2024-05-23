88a5436026d7a636403f66018bd3ce1d5fd51fe4
monisha-1511 <monishav415@gmail.com>
2024-05-23 12:44:05 +0530

    class project added

---
+++ src/test/java/org/tcs/Mvn.java
@@ -0,0 +1,29 @@
+package org.tcs;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class Mvn {
+	public static void main(String[] args) throws IOException {
+
+		File file = new File("C:\\Users\\Lenovo\\eclipse-workspace\\Class\\excelsheet\\LOGIN.xlsx");
+		FileInputStream fI = new FileInputStream(file);
+		Workbook w = new XSSFWorkbook(fI);
+
+		Sheet sheet = w.getSheet("LOGIN");
+		Row row = sheet.getRow(2);
+
+		Cell cell = row.getCell(0);
+		System.out.println(cell);
+
+	}
+
+}
